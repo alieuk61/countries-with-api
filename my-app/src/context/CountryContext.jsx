4f55d46cc02439f9c1c920e7cdc01572ded57716
selly361 <seikou615@gmail.com>
2023-12-21 21:39:48 +0000

    Add: search country functionality

--- my-app/src/context/CountryContext.jsx
+++ my-app/src/context/CountryContext.jsx
@@ -1,57 +1,79 @@
 import { createContext, useContext, useEffect, useState } from 'react'
-import * as api from "../components/apiCalls";
+import * as api from '../components/apiCalls'
 
 export const CountryContext = createContext({})
 
 function CountryContextProvider({ children }) {
+  const [countryData, setCountryData] = useState({})
+  const [currentPage, changePage] = useState('main-page')
+  const [data, setData] = useState([])
+  const [countryInput, setCountryInput] = useState('')
 
-    const [countryData, setCountryData] = useState({});
-    const [currentPage, changePage] = useState('main-page');
-    const [data, setData] = useState([]);
+  function pageChanger(e) {
+    changePage(e.target.textContent)
+  }
 
-    function pageChanger(e) {
-        changePage(e.target.textContent);
+  async function fetchCountry() {
+    const countries = await api.getCountry(countryInput)
+
+    if(countries){
+        setData(countries)
     }
-    
+  }
 
-    useEffect(() => {
-        const fetchData = async () => {
-            switch (currentPage) {
-                case 'Africa':
-                    setData(await api.getDataAfrica()); 
-                    break;
-                case 'America':
-                    setData(await api.getDataAmericas());
-                    break;
-                case 'Asia':
-                    setData(await api.getDataAsia());
-                    break;
-                case 'Europe':
-                    setData(await api.getDataEurope());
-                    break;
-                case 'Oceania':
-                    setData(await api.getDataOceania());
-                    break;
-                default:
-                    setData(await api.getMainPageData());
-            }
-        };
 
-        fetchData();
-    }, [currentPage]);
+  const changeData = async(request) => setData(await request());
 
-	return (
-		<CountryContext.Provider
-			value={{
-                countryData,
-                setCountryData,
-                pageChanger,
-                data
-			}}
-		>
-			{children}
-		</CountryContext.Provider>
-	)
+
+  useEffect(() => {
+    fetchCountry()
+
+  }, [countryInput])
+
+  if(!data.length || !countryInput) {
+    changeData(api.getMainPageData)
+}
+
+  useEffect(() => {
+    const fetchData = async () => {
+      switch (currentPage) {
+        case 'Africa':
+          changeData(api.getDataAfrica)
+          break
+        case 'America':
+          changeData(api.getDataAmericas)
+          break
+        case 'Asia':
+          changeData(api.getDataAsia)
+          break
+        case 'Europe':
+          changeData(api.getDataEurope)
+          break
+        case 'Oceania':
+          changeData(api.getDataOceania)
+          break
+        default:
+          changeData(api.getMainPageData)
+      }
+    }
+
+    fetchData()
+  }, [currentPage])
+
+  return (
+    <CountryContext.Provider
+      value={{
+        countryData,
+        setCountryData,
+        pageChanger,
+        data,
+        countryInput,
+        setCountryInput,
+      }}
+    >
+      {children}
+    </CountryContext.Provider>
+  )
 }
 
 export default CountryContextProvider
