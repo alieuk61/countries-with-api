dca8788b28df58553b0f96d194499f1d6a0fd5b2
selly361 <seikou615@gmail.com>
2023-12-21 21:39:08 +0000

    Change: refactored

--- my-app/src/components/searchBar.jsx
+++ my-app/src/components/searchBar.jsx
@@ -1,15 +1,19 @@
 import TextField from '@mui/material/TextField';
-import {useState} from 'react';
+import {useContext, useState} from 'react';
+import { CountryContext } from '../context/CountryContext';
 
 export default function SearchBar(){
+
+    const { countryInput, setCountryInput  } = useContext(CountryContext);
 
     return(
     <TextField 
     className='shadow-lg bg-white w-[450px]' 
     id="outlined-basic" variant="outlined"
-    // onChange={(e) => {
-    //     // console.log(e.target.value)
-    // }}
+    value={countryInput}
+    onChange={(e) => {
+        setCountryInput(e.target.value)
+    }}
     />
     )
 };
